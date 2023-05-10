6da7ae104e5555288a18a58144279a7a467e8c2a
day48 <125416805+day48@users.noreply.github.com>
2023-05-09 22:54:06 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,39 @@
-package br.com.redesocial;
+package br.com;
 
-import java.util.ArrayList;
-import javax.swing.JOptionPane ;
+import javax.swing.JOptionPane;
 
-public class Main {
-    public static void main(String[] args) {
-        String dataCriacao,nomeusuario,datanascimento,senha;
-        String dataPublicacao,textoPublicacao,linkmidia;
+    public class Main {
+        public static void main(String[] args) {
+            Funcionario[] funcionarios = new Funcionario[10];
+            Funcionario func;
+            Empresa emp1;
+            String nome,email,telefone,departamento,dataEntrada,RG, nomeEmp,CNPJ;
+            Double salario;
+            int qtdeFunc;
+            boolean status=true;
 
-        dataCriacao= JOptionPane.showInputDialog("Digite a data de criacao de rede social");
-        nomeusuario= JOptionPane.showInputDialog("Digite o nome do usuaerio");
-        datanascimento= JOptionPane.showInputDialog("Digite a data do usuario");
-        senha= JOptionPane.showInputDialog("Digite a senha");
+            for (int i=0;i<2;i++) {
+                nome=JOptionPane.showInputDialog("Nome");
+                email=JOptionPane.showInputDialog("Email");
+                telefone=JOptionPane.showInputDialog("Telefone");
+                departamento=JOptionPane.showInputDialog("Departamento");
+                dataEntrada=JOptionPane.showInputDialog("dataEntrada");
+                RG=JOptionPane.showInputDialog("RG");
+                salario=Double.parseDouble(JOptionPane.showInputDialog("Salario"));
+                func = new Funcionario(nome,email,telefone,departamento,salario,dataEntrada,RG,status);
+                funcionarios[i]=func;
+            }
 
-         RedeSocial redeSocial = new RedeSocial(dataCriacao,nomeusuario,datanascimento,senha);
-         Publicacao publi1;
+            nomeEmp=JOptionPane.showInputDialog("Criando a Empresa, digite Nome");
+            CNPJ=JOptionPane.showInputDialog("CNPJ");
+            qtdeFunc=Integer.parseInt(JOptionPane.showInputDialog("Quantidade de Funcionario"));
+            emp1 = new Empresa(nomeEmp,CNPJ,qtdeFunc);
+            for(Funcionario func1:funcionarios) {
+                emp1.insereFuncionario(func1);
+                System.out.println(func1.mostrarDados());
+            }
 
-         ArrayList<Publicacao>publi = new ArrayList<>();
-
-         for (int i =0; i<3;i++){
-
-             dataPublicacao =JOptionPane.showInputDialog("Digite a data da publicacao");
-             textoPublicacao= JOptionPane.showInputDialog("Digite o texto");
-             linkmidia = JOptionPane.showInputDialog("Digite o link");
-
-             publi1 = new Publicacao( dataPublicacao,textoPublicacao,linkmidia);
-             publi.add(publi1); //adiciona obj no arraylist.
-             redeSocial.inserePublicacao(publi1); //adiciona o obj na rede social .
-
-         }
-         redeSocial.imprimePublicacoes();
-
-
+        }
 
     }
-}
+
